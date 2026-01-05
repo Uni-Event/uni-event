8a2de5cd78aedf2d958bc624aa4ed6bc08379e87
AlexandriucIonut <ionut.alexandriuc@student.usv.ro>
2026-01-05 19:01:26 +0200

    feat(frontend): Create Event button functionality

--- frontend/src/pages/OrganizerHomePage.jsx
+++ frontend/src/pages/OrganizerHomePage.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Layout from '../components/Layout'; 
+import React from "react";
+import Layout from "../components/Layout";
 
 function OrganizerHomePage() {
   return (
     <Layout>
       <div>OrganizerHomePage</div>
     </Layout>
-  )
+  );
 }
 
-export default OrganizerHomePage
+export default OrganizerHomePage;
